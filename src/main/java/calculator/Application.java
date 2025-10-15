a189c23d0e5fff8e6ea32f6834f02efcd3138314
hyun.lee <qwe7105c@naver.com>
2025-10-15 16:17:06 +0900

    feat: 커스텀 구분자 지원 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,18 +4,27 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        
         String input = Console.readLine();
         String delimiter = "[,:]";
 
+        
+        if(input.startsWith("//") && input.contains("\\n")) {
+            int newlineIndex = input.indexOf("\\n");
+            String customDelimiter = input.substring(2, newlineIndex);
+
+            input = input.substring(newlineIndex + 2);
+            delimiter = "[,:]|" + customDelimiter;
+        }
         String[] numbers = input.split(delimiter);
         int sum = 0;
-
+        
         for(String number : numbers){
             if(!number.trim().isEmpty()){
                 sum += Integer.parseInt(number.trim());
             }
         }
-
+        
         System.out.println("결과 : " + sum);
     }
 }
